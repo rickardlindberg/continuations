f0b3113b4dcc0bd9d94694c1a6e8f5246ad61967
Rickard Lindberg <ricli85@gmail.com>
2012-09-02 09:34:01 +0200

    simplify types with typedefs

--- pythagoras.c
+++ pythagoras.c
@@ -20,33 +20,37 @@ struct env_pair {
     void * value;
 };
 
+typedef struct env_pair * EnvPair;
+
 struct env {
     int size;
-    struct env_pair * entries;
+    EnvPair entries;
     struct env * parent;
 };
 
-struct env * env_create(int size, struct env * parent) {
-    struct env * env = (struct env *)malloc(sizeof(struct env));
+typedef struct env * Env;
+
+Env env_create(int size, Env parent) {
+    Env env = (Env)malloc(sizeof(struct env));
     env->size = size;
-    env->entries = (struct env_pair *)malloc(sizeof(struct env_pair) * size);
+    env->entries = (EnvPair)malloc(sizeof(struct env_pair) * size);
     env->parent = parent;
     return env;
 }
 
-void env_destroy(struct env * env) {
+void env_destroy(Env env) {
     free(env->entries);
     free(env);
 }
 
-void env_insert(struct env * env, int i, char * key, void * value) {
+void env_insert(Env env, int i, char * key, void * value) {
     struct env_pair pair;
     pair.key = key;
     pair.value = value;
     env->entries[i] = pair;
 }
 
-void * env_lookup(struct env * env, char * name) {
+void * env_lookup(Env env, char * name) {
     int i;
     if (env == NULL) {
         return NULL;
@@ -66,89 +70,85 @@ struct args {
     void ** args;
 };
 
-struct args * args_create(int size) {
-    struct args * args = (struct args *)malloc(sizeof(struct args));
+typedef struct args * Args;
+
+Args args_create(int size) {
+    Args args = (Args)malloc(sizeof(struct args));
     args->size = size;
     args->args = (void **)malloc(sizeof(void *) * size);
     return args;
 }
 
-void args_destroy(struct args * args) {
+void args_destroy(Args args) {
     free(args->args);
     free(args);
 }
 
-void args_insert(struct args * args, int i, void * value) {
+void args_insert(Args args, int i, void * value) {
     args->args[i] = value;
 }
 
-void * args_get(struct args * args, int i) {
+void * args_get(Args args, int i) {
     return args->args[i];
 }
 
-// FRAME
+// FUNCTION STUFF
 
-//struct fn {
-//    void (*fn)(struct env *env);
-//    struct env *env;
-//};
-//
-//struct frame {
-//    struct fn *fn;
-//    void * args[10];
-//};
-//
-//void * arg(struct frame *frame, int n) {
-//    return frame->args[n];
+typedef struct call (*FnPtr)(Env env, Args args);
+
+struct fn {
+    FnPtr fn_spec;
+    Env env;
+};
+
+struct fn fn_new(FnPtr fn_spec, Env env) {
+    struct fn fn;
+    fn.fn_spec = fn_spec;
+    fn.env = env;
+    return fn;
+};
+
+struct call {
+    struct fn fn;
+    struct args args;
+};
+
+struct call call_new(struct fn fn, struct args args) {
+    struct call call;
+    call.fn = fn;
+    call.args = args;
+    return call;
+}
+
+// BUILTIN
+
+//struct call * builtin_exit(Env env, Args args) {
+//    return NULL;
 //}
-//
-//// BUILTIN
-//
-//void builtin_exit(struct frame *frame) {
-//    frame->fn = 0;
-//}
-//
-//void builtin_print_number(struct frame *frame) {
-//    printf("%d", (char *)arg(frame, 0));
+
+//struct call * builtin_print(Env env, Args args) {
+//    printf((char*)arg(frame, 0));
 //    frame->fn = arg(frame, 1);
 //}
-//
-//void builtin_plus(struct frame *frame) {
-//    int res = *(int *)arg(frame, 0) + *(int *)arg(frame, 1);
-//    frame->fn = arg(frame, 2);
-//    frame->args[0] = &res;
-//}
-//
-//// COMPILED
-//
-//struct fn * compiled_main(struct env * env) {
-//    struct fn * pythagoras = env_lookup(env, "pythagoras");
-//    int n1 = 2;
-//    int n2 = 4;
-//    (struct fn *)(strucnt env *) anon = &compiled_anonymous_res;
-//}
-//
-//struct fn * compiled_anonymous_res(struct env * env) {
-//}
-//
-//struct fn * compiled_pythagoras(struct env * env) {
-//}
-//
-//struct fn * compiled_anonymous_times_x(struct env * env) {
-//}
-//
-//struct fn * compiled_anonymous_times_y(struct env * env) {
-//}
-//
-//struct fn * compiled_anonymous_sum(struct env * env) {
+
+// COMPILED
+
+//struct call * compiled_main(Env env, Args args) {
+//    char* s = "hello world\n";
+//    Args args = args_create(2);
+//    // prepare args
+//    args_insert(args, 0, s);
+//    args_insert(args, 1, &builtin_exit);
+//    // prepare call
+//    &builtin_print
 //}
 
-// LOOP
+// TESTS
 
 void test_env() {
     char * v1 = "value1";
     char * v2 = "value2";
-    struct env * env = env_create(2, NULL);
+    Env env = env_create(2, NULL);
     printf("Testing env\n");
     env_insert(env, 0, "key1", v1);
     env_insert(env, 1, "key2", v2);
@@ -161,7 +161,7 @@ void test_env() {
 void test_args() {
     char * a1 = "arg 1";
     char * a2 = "arg 2";
-    struct args * args = args_create(2);
+    Args args = args_create(2);
     printf("Testing args\n");
     args_insert(args, 0, a1);
     args_insert(args, 1, a2);
@@ -171,16 +171,15 @@ void test_args() {
     args_destroy(args);
 }
 
+// LOOP
+
 int main() {
     test_env();
     test_args();
 
-//    struct env env;
-//
-//    struct frame frame;
-//    frame.fn = &compiled_main;
-//    while (frame.fn != 0) {
-//        (*frame.fn)(&frame);
-//    }
-    return 1;
+    Env global_env = env_create(0, NULL);
+
+    while (1) {
+        return 1;
+    }
 }
