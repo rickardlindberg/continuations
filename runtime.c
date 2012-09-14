2b46ffd949fe883fbe2d8272e0b68a06ab552b3c
Rickard Lindberg <ricli85@gmail.com>
2012-09-14 10:44:25 +0200

    implement freeing with reference counting

--- runtime.c
+++ runtime.c
@@ -1,22 +1,52 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #include "runtime.h"
+
+// Ref countable
+
+void free_ref_countable(void * ref_countable) {
+    RefCount ref_count = (RefCount)ref_countable;
+    if (ref_count->count == 0) {
+        ref_count->free_fn(ref_countable);
+    }
+}
+
+static void dec_and_free(void * ref_countable) {
+    free_ref_countable(dec(ref_countable));
+}
+
+static void * inc(void * ref_countable) {
+    RefCount ref_count = (RefCount)ref_countable;
+    ref_count->count++;
+    return ref_countable;
+}
+
+static void * dec(void * ref_countable) {
+    RefCount ref_count = (RefCount)ref_countable;
+    ref_count->count--;
+    return ref_countable;
+}
 
 // Env
 
 Env create_env(Env parent) {
     Env env = (Env)malloc(sizeof(struct env));
+    env->ref_count.count = 0;
+    env->ref_count.free_fn = &free_env;
     env->size = 0;
     env->pairs = (struct pair *)malloc(env->size * sizeof(struct pair));
     env->parent = parent;
+    if (parent != NULL) {
+        inc(parent);
+    }
     return env;
 }
 
-void env_insert(Env env, char * key, void * value) {
+void env_insert(Env env, char * key, void * ref_countable) {
     struct pair pair;
     pair.key = key;
-    pair.value = value;
+    pair.ref_countable = inc(ref_countable);
     env->size++;
     env->pairs = (struct pair *)realloc(env->pairs, env->size * sizeof(struct pair));
     env->pairs[env->size - 1] = pair;
@@ -30,87 +60,136 @@ void * env_lookup(Env env, char * key) {
     }
     for (i = 0; i < env->size; i++) {
         if (strcmp(env->pairs[i].key, key) == 0) {
-            return env->pairs[i].value;
+            return env->pairs[i].ref_countable;
         }
     }
     return env_lookup(env->parent, key);
+}
+
+static void free_env(void * ref_countable) {
+    Env env = (Env)ref_countable;
+    int i;
+    for (i = 0; i < env->size; i++) {
+        dec_and_free(env->pairs[i].ref_countable);
+    }
+    free(env->pairs);
+    if (env->parent != NULL) {
+        dec_and_free(env->parent);
+    }
+    free(env);
 }
 
 // Args
 
 Args create_args(int size) {
     Args args = (Args)malloc(sizeof(struct args));
+    args->ref_count.count = 0;
+    args->ref_count.free_fn = &free_args;
     args->size = size;
     args->args = (void **)malloc(args->size * sizeof(void *));
     return args;
+}
+
+void args_set(Args args, int i, void * ref_countable) {
+    args->args[i] = inc(ref_countable);
 }
 
 void * args_get(Args args, int i) {
     return args->args[i];
 }
 
-void args_set(Args args, int i, void * value) {
-    args->args[i] = value;
+static void free_args(void * ref_countable) {
+    Args args = (Args)ref_countable;
+    int i;
+    for (i = 0; i < args->size; i++) {
+        dec_and_free(args->args[i]);
+    }
+    free(args->args);
+    free(args);
 }
 
 // Closure
 
 Closure create_closure(FnSpec fn_spec, Env env) {
     Closure closure = (Closure)malloc(sizeof(struct closure));
+    closure->ref_count.count = 0;
+    closure->ref_count.free_fn = &free_closure;
     closure->fn_spec = fn_spec;
-    closure->env = env;
+    closure->env = inc(env);
     return closure;
+}
+
+static void free_closure(void * ref_countable) {
+    Closure closure = (Closure)ref_countable;
+    dec_and_free(closure->env);
+    free(closure);
 }
 
 // Call
 
 Call create_call(Closure closure, Args args) {
     Call call = (Call)malloc(sizeof(struct call));
-    call->closure = closure;
-    call->args = args;
+    call->ref_count.count = 0;
+    call->ref_count.free_fn = &free_call;
+    call->closure = inc(closure);
+    call->args = inc(args);
     return call;
+}
+
+static void free_call(void * ref_countable) {
+    Call call = (Call)ref_countable;
+    dec_and_free(call->closure);
+    dec_and_free(call->args);
+    free(call);
 }
 
 // Constants
 
-void * const_number(double i) {
-    double * value = (double *)malloc(sizeof(double));
-    *value = i;
-    return (void *)value;
+Number const_number(double i) {
+    Number number = (Number)malloc(sizeof(struct number));
+    number->ref_count.count = 0;
+    number->ref_count.free_fn = &free_number;
+    number->value = i;
+    return number;
+}
+
+static void free_number(void * ref_countable) {
+    Number number = (Number)ref_countable;
+    free(number);
 }
 
 // Built-ins
 
 Call builtin_times(Env env, Args args) {
-    double * left = (double *)args_get(args, 0);
-    double * right = (double *)args_get(args, 1);
+    Number left = (Number)args_get(args, 0);
+    Number right = (Number)args_get(args, 1);
     Closure k = (Closure)args_get(args, 2);
     Args next_args = create_args(1);
-    args_set(next_args, 0, const_number((*left) * (*right)));
+    args_set(next_args, 0, const_number(left->value * right->value));
     return create_call(k, next_args);
 }
 
 Call builtin_plus(Env env, Args args) {
-    double * left = (double *)args_get(args, 0);
-    double * right = (double *)args_get(args, 1);
+    Number left = (Number)args_get(args, 0);
+    Number right = (Number)args_get(args, 1);
     Closure k = (Closure)args_get(args, 2);
     Args next_args = create_args(1);
-    args_set(next_args, 0, const_number((*left) + (*right)));
+    args_set(next_args, 0, const_number(left->value + right->value));
     return create_call(k, next_args);
 }
 
 Call builtin_sqrt(Env env, Args args) {
-    double * n = (double *)args_get(args, 0);
+    Number n = (Number)args_get(args, 0);
     Closure k = (Closure)args_get(args, 1);
     Args next_args = create_args(1);
-    args_set(next_args, 0, const_number(sqrt(*n)));
+    args_set(next_args, 0, const_number(sqrt(n->value)));
     return create_call(k, next_args);
 }
 
 Call builtin_printNumber(Env env, Args args) {
-    double * n = (double *)args_get(args, 0);
+    Number n = (Number)args_get(args, 0);
     Closure k = (Closure)args_get(args, 1);
-    printf("%f\n", *n);
+    printf("%f\n", n->value);
     return create_call(k, create_args(0));
 }
 
--- test-runtime.c
+++ test-runtime.c
@@ -5,30 +5,30 @@
 void test_env() {
     Env env1 = create_env(NULL);
     Env env2 = create_env(env1);
-    char * value1 = "v1";
-    char * value2 = "v2";
-    char * value3 = "v3";
+    Number value1 = const_number(1);
+    Number value2 = const_number(2);
+    Number value3 = const_number(3);
 
     env_insert(env1, "foo", value1);
     env_insert(env2, "bar", value2);
     env_insert(env2, "baz", value3);
 
-    printf("%s\n", env_lookup(env2, "foo"));
-    printf("%s\n", env_lookup(env2, "bar"));
-    printf("%s\n", env_lookup(env2, "baz"));
+    printf("%f\n", ((Number)env_lookup(env2, "foo"))->value);
+    printf("%f\n", ((Number)env_lookup(env2, "bar"))->value);
+    printf("%f\n", ((Number)env_lookup(env2, "baz"))->value);
     //printf("%s\n", env_lookup(env2, "foobar"));
 }
 
 void test_args() {
     Args args = create_args(2);
-    char * value1 = "arg1";
-    char * value2 = "arg2";
+    Number value1 = const_number(1);
+    Number value2 = const_number(2);
 
     args_set(args, 0, value1);
     args_set(args, 1, value2);
 
-    printf("%s\n", args_get(args, 0));
-    printf("%s\n", args_get(args, 1));
+    printf("%f\n", ((Number)args_get(args, 0))->value);
+    printf("%f\n", ((Number)args_get(args, 1))->value);
     //printf("%s\n", args_get(args, 2));
 }
 
