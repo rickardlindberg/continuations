ef407be14f2f74a97bee6afb5333996bb8cf8bec
Rickard Lindberg <ricli85@gmail.com>
2012-09-19 20:32:06 +0200

    extract CPC module

--- compiler/compiler/Compiler.hs
+++ compiler/compiler/Compiler.hs
@@ -1,49 +1,7 @@
-import Stages.Analyze (syntaxToSemantic)
-import Stages.CodeGen (generateCode)
-import Stages.Parser (translate)
-import System
-import System.Directory
-import System.FilePath
-import System.Process
-import Text.ParserCombinators.Parsec (parse)
+import Runtime.CPC (compile)
+import System (getArgs)
 
 main :: IO ()
 main = do
-
-    [srcFilePath] <- getArgs
-
-    let runtimeDir = "runtime"
-
-    let runtimeH   = runtimeDir </> "runtime.h"
-    let runtimeC   = runtimeDir </> "runtime.c"
-
-    let buildDir   = takeDirectory srcFilePath </>
-                     takeBaseName srcFilePath ++ "-conc-build"
-
-    let destFile   = buildDir </>
-                     takeBaseName srcFilePath ++ ".c"
-
-    input <- readFile srcFilePath
-    case parse translate srcFilePath input of
-        Left  error   -> print error
-        Right program -> do
-
-            createDirectoryIfMissing True buildDir
-
-            writeFile destFile (generateCode (syntaxToSemantic program))
-
-            copyFile runtimeH (buildDir </> "runtime.h")
-            copyFile runtimeC (buildDir </> "runtime.c")
-
-            let makefile = buildDir </> "Makefile"
-            let oo = dropExtension (takeBaseName destFile)
-            let makefileLines = [ oo ++ ": " ++ takeFileName destFile ++ " runtime.c runtime.h\n"
-                                , "\t gcc -lm -o " ++ oo ++ " " ++ takeFileName destFile ++ " runtime.c\n"
-                                ]
-
-            writeFile makefile (concat makefileLines)
-
-            setCurrentDirectory buildDir
-
-            res <- rawSystem "make" []
-            exitWith res
+    [filepath] <- getArgs
+    compile filepath
---
+++ compiler/compiler/Runtime/CPC.hs
@@ -0,0 +1,51 @@
+module Runtime.CPC where
+
+import Stages.Analyze (syntaxToSemantic)
+import Stages.CodeGen (generateCode)
+import Stages.Parser (translate)
+import System
+import System.Directory
+import System.FilePath
+import System.Process
+import Text.ParserCombinators.Parsec (parse)
+
+runtime = "cpc"
+
+compile :: FilePath -> IO ()
+compile srcFilePath = do
+
+    let runtimeDir = "runtime"
+
+    let runtimeH   = runtimeDir </> "runtime.h"
+    let runtimeC   = runtimeDir </> "runtime.c"
+
+    let buildDir   = takeDirectory srcFilePath </>
+                     takeBaseName srcFilePath ++ "-conc-build-" ++ runtime
+
+    let destFile   = buildDir </>
+                     takeBaseName srcFilePath ++ ".c"
+
+    input <- readFile srcFilePath
+    case parse translate srcFilePath input of
+        Left  error   -> print error
+        Right program -> do
+
+            createDirectoryIfMissing True buildDir
+
+            writeFile destFile (generateCode (syntaxToSemantic program))
+
+            copyFile runtimeH (buildDir </> "runtime.h")
+            copyFile runtimeC (buildDir </> "runtime.c")
+
+            let makefile = buildDir </> "Makefile"
+            let oo = dropExtension (takeBaseName destFile)
+            let makefileLines = [ oo ++ ": " ++ takeFileName destFile ++ " runtime.c runtime.h\n"
+                                , "\t gcc -lm -o " ++ oo ++ " " ++ takeFileName destFile ++ " runtime.c\n"
+                                ]
+
+            writeFile makefile (concat makefileLines)
+
+            setCurrentDirectory buildDir
+
+            res <- rawSystem "make" []
+            exitWith res
